a8e1350d189a79528536f2d988160a50cc6c1122
Quasar Jarosz <quasar@uams.edu>
2024-03-08 14:11:20 -0600

    New webpack config to replace all vite functions

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,34 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  mode: 'development',
   entry: './src/nifti.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
+  devtool: 'inline-source-map',
+  devServer: {
+    static: ['dist', 'public'],
+    headers: {
+      "Cross-Origin-Opener-Policy": "same-origin", 
+      "Cross-Origin-Embedder-Policy": "require-corp", 
+    },
+    proxy: [
+      {
+        context: ['/papi'],
+        target: 'http://tcia-posda-rh-1.ad.uams.edu',
+        headers: { 'Authorization': 'Bearer e9a63bc2-bfa5-4299-afb3-c844fb2ef38b' },
+      },
+    ],
   },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+	clean: true,
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html'
+    })
+  ],
   experiments: {
     asyncWebAssembly: true,
     // syncWebAssembly: true,
@@ -13,4 +36,7 @@ module.exports = {
   module: {
     rules: [{ test: /\.wasm/, type: 'asset/resource' }]
   },
+  // optimization: {
+  //   runtimeChunk: 'single',
+  // },
 };
